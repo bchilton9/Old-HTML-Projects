fb979a03a82dcf498ff888e3657a9e8b31047732
bchilton9 <48000771+bchilton9@users.noreply.github.com>
2025-07-11 08:41:01 -0600

    rename

---
+++ SoS/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Seekers of Serenity</title>
+<style type="text/css" media="all">
+<!--
+@import url("screen.css");
+-->
+</style>
+</head>
+<body>
+<table width="100%" height="100%">
+<tr>
+<td align="center" valign="center"><a href="index.pl"><img src="images/charter.jpg"></a>
+</td>
+</tr>
+</table>
+</body>
+</html>
